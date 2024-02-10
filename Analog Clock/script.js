77e4e8a9efa62ca8b77b1f077ad7701fcae6e1f1
Madhukar kumar <madhukarkumar2468@gmail.com>
2024-02-10 11:40:19 +0530

    code

--- Analog Clock/script.js
+++ Analog Clock/script.js
@@ -8,6 +8,15 @@ function displayTime(){
     let hh = date.getHours();
     let mm = date.getMinutes();
     let ss = date.getSeconds();
+
+    let hRotation = 30*hh + mm/2;
+    let mRotation = 6*mm;
+    let sRotation = 6*ss;
+
+    hr.style.transform = `rotate(${hRotation}deg)`;
+    min.style.transform = `rotate(${mRotation}deg)`;
+    sec.style.transform = `rotate(${sRotation}deg)`;
+
 }
 
 setInterval(displayTime,1000);

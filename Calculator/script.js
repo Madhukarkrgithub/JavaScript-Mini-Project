f040ac2d2aa6b90657366df703076b72fd7af22e
Madhukar kumar <madhukarkumar2468@gmail.com>
2024-02-12 02:09:44 +0530

    code

--- Calculator/script.js
+++ Calculator/script.js
@@ -80,3 +80,59 @@ function resetScreen() {
     lastOperationScreen.textContent = `${firstOperand} ${currentOperation} ${secondOperand} =`;
     currentOperation = null;
   }
+  
+function roundResult(number) {
+    return Math.round(number * 1000) / 1000;
+  }
+  
+  function handleKeyboardInput(e) {
+    if (e.key >= 0 && e.key <= 9) appendNumber(e.key);
+    if (e.key === '.') appendPoint();
+    if (e.key === '=' || e.key === 'Enter') evaluate();
+    if (e.key === 'Backspace') deleteNumber();
+    if (e.key === 'Escape') clear();
+    if (e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/')
+      setOperation(convertOperator(e.key));
+  }
+  
+  function convertOperator(keyboardOperator) {
+    if (keyboardOperator === '/') return '÷';
+    if (keyboardOperator === '*') return '×';
+    if (keyboardOperator === '-') return '−';
+    if (keyboardOperator === '+') return '+';
+  }
+  
+  function add(a, b) {
+    return a + b;
+  }
+  
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  function multiply(a, b) {
+    return a * b;
+  }
+  
+  function divide(a, b) {
+    return a / b;
+  }
+  
+  function operate(operator, a, b) {
+    a = Number(a);
+    b = Number(b);
+    switch (operator) {
+      case '+':
+        return add(a, b);
+      case '−':
+        return subtract(a, b);
+      case '×':
+        return multiply(a, b);
+      case '÷':
+        if (b === 0) return null;
+        else return divide(a, b);
+      default:
+        return null;
+    }
+  }
+  
